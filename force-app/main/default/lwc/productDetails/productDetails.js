6ab3bc9072bb66aef3c9b658306fa6fc4345f946
Mohamed Ali <msallam963963@gmail.com>
2023-12-09 15:40:17 +0200

    Some Changes & Add NPV Claculator

--- force-app/main/default/lwc/productDetails/productDetails.js
+++ force-app/main/default/lwc/productDetails/productDetails.js
@@ -4,18 +4,21 @@ import getProductDetails from '@salesforce/apex/ProductController.getProductDeta
 
 export default class ProductDetails extends LightningElement {
     @api productId;
+    
     productName;
 
+    
     // Use a wire service to fetch details based on the selected product's ID
     @wire(getProductDetails, { productId: '$productId' })
     wiredProduct({ error, data }) {
         if (data) {
             // Assign the fetched product details to properties
             this.productName = data.Name;
-            console.log('Fetched Product Details:', data);
-            console.log('Fetched Product Details:', data.Name);
+            console.log('Fetched Product Details:--------------', data);
+            console.log('Fetched Product Details:-------------', data.Name);
+            console.log('Fetched Product Details:-------------', productName);
         } else if (error) {
-            // Handle error
+            console.log('Error:-------------', error);
         }
     }
 }
